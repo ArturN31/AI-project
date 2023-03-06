3518174731b9dca3b91024e354682cdbe8bf4964
ArturN31 <ArturN311@gmail.com>
2023-03-06 19:54:38 +0000

    Changed API and fixed outputs

--- src/components/News/NewsSentiment.js
+++ src/components/News/NewsSentiment.js
@@ -1,46 +1,59 @@
 import React, { useEffect, useState } from "react"
-import { Row, Col } from "react-bootstrap";
 
 const NewsSentiment = (text) => {
     const [sentimentAnalysis, setSentimentAnalysis] = useState([]);
 
     useEffect(() => {
-        if (!text.length > 0) { 
-            //sentiment analysis
-            const optionsSentiment = {
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json',
-                    'X-RapidAPI-Key': process.env.REACT_APP_TEXT_ANALYSIS_API_KEY,
-                    'X-RapidAPI-Host': 'text-analysis12.p.rapidapi.com'
-                },
-                body: '{"language":"english","text":"'+text.text+'"}'
-            };
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Key': process.env.REACT_APP_TEXTPROBE_API_KEY,
+                'X-RapidAPI-Host': 'textprobe.p.rapidapi.com'
+            },
+            body: '{"text":"' + text.text + '"}'
+        };
 
-            fetch('https://text-analysis12.p.rapidapi.com/sentiment-analysis/api/v1.1', optionsSentiment)
-            .then((response) => {
+        if(text.text !== "") { 
+            fetch('https://textprobe.p.rapidapi.com/feelings', options)
+             .then((response) => {
                 if (!response.ok) {throw response} 
                 return response.json();
             })
             .then((incomingData) => {
                 setSentimentAnalysis(incomingData);
             }) 
-            .catch((err) => console.error(err))  
+            .catch((err) => console.error(err)) 
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[]);
+    },[text.text]);
 
     return (
-        sentimentAnalysis.length >= 0
-        ?   <p className="text-center"> Processing {text.text.split(".").length} sentences - please wait until sentiment analysis is completed.</p>
-
+        sentimentAnalysis.length <= 0
+        ?   <>
+                {text.text.length <= 0
+                    ? ""
+                    : "Loading analysis..."
+                }
+            </>
         :   <>
-                <h6 className="text-center">Sentiment analysis:</h6>
-                <Row>
-                    <Col>
-                        <p>{sentimentAnalysis.sentiment}</p>
-                    </Col>
-                </Row> 
+                <h5>Sentiment analysis:</h5>
+                {sentimentAnalysis.emotion_prediction && sentimentAnalysis.emotion_scores.Anger
+                    ?   <>
+                            <p>Emotion prediction: <span> {sentimentAnalysis.emotion_prediction}</span></p>
+                            <p>Anger: <span> {sentimentAnalysis.emotion_scores.Anger}</span></p>
+                            <p>Fear: <span> {sentimentAnalysis.emotion_scores.Fear}</span></p>
+                            <p>Joy: <span> {sentimentAnalysis.emotion_scores.Joy}</span></p>
+                            <p>Neutral: <span> {sentimentAnalysis.emotion_scores.Neutral}</span></p>
+                            <p>Sadness: <span> {sentimentAnalysis.emotion_scores.Sadness}</span></p>
+                            <p>Negative:  <span> {sentimentAnalysis.sentiment_scores.Negative}</span></p>
+                            <p>Positive: <span> {sentimentAnalysis.sentiment_scores.Positive}</span></p>
+                        </>
+                    :   <>
+                            <p>Emotion prediction: <span> {sentimentAnalysis.emotion_prediction}</span> - cannot be processed</p>
+                            <p>Negative:  <span> {sentimentAnalysis.sentiment_scores.Negative}</span></p>
+                            <p>Positive: <span> {sentimentAnalysis.sentiment_scores.Positive}</span></p>
+                        </>
+                }
             </>
     )
 }
